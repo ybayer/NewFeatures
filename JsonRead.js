e11d704aa6ce5c5e2c38c059b200b07ecc27c209
Yoko <ybayer@adobe.com>
2013-05-20 15:41:08 -0700

    updated JSON file to clarify the sample data structure of JSON file

--- JsonRead.js
+++ JsonRead.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     //JQuery API to load JSON-encoded data
     $.getJSON('data.json', function(featureList) {
         
+        //alert(featureList.buildNumber);
         //alert(featureList.newFeatures[1].name);
           
         $(featureList.newFeatures).each(function(index, items){ 
 
-            $('#features').append('<tr> <td nowrap> '+featureList.newFeatures[index].name+' </td> <td width="800px"> '+featureList.newFeatures[index].description+' </td></tr>');     
+            $('#features').append('<tr> <td nowrap> '+featureList.newFeatures[index].feature_name+' </td> <td width="800px"> '+featureList.newFeatures[index].feature_desc+' </td></tr>');     
         })//each
     
     });//getJSON
