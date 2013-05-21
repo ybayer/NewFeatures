ae1c2353755746067d9c8c35cf9ef00c118850d2
Yoko <ybayer@adobe.com>
2013-05-20 21:51:18 -0700

    added code to localize the title and table headers. Enhanced fonts in CSS

--- JsonRead.js
+++ JsonRead.js
@@ -1,11 +1,18 @@
 $(document).ready(function() { 
-    
+
     //JQuery API to load JSON-encoded data
     $.getJSON('data.json', function(featureList) {
         
-        //alert(featureList.buildNumber);
-        //alert(featureList.newFeatures[1].name);
-          
+        //update title
+        var output = '<h3>' + featureList.title + '</h3>';
+        $('#headline').html(output);
+        
+        //update table headers
+        output='<tr id="header"> <th scope="col">' + featureList.header_name + '</th>'+'<th scope="col">' + featureList.header_desc + '</th> </tr>';
+
+        $('#features').append(output);  
+                              
+        //update table
         $(featureList.newFeatures).each(function(index, items){ 
 
             $('#features').append('<tr> <td nowrap> '+featureList.newFeatures[index].feature_name+' </td> <td width="800px"> '+featureList.newFeatures[index].feature_desc+' </td></tr>');     
